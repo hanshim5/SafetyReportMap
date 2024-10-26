e8b3f489001b0b240047cb291db08237065e3e45
jaerlcruz <jaeraelcruz@gmail.com>
2024-10-26 13:27:12 -0700

    Added safety resource information

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -15,9 +15,13 @@ function SideBar() {
           {/* TODO: Use conditional to see if marker is selected. */}
           <p className="text-gray-500">Select a marker to see incident details.</p>
         </div>
-        <div className="text-black">
+        <div className="text-black bg-slate-200 m-4 p-4 rounded-lg">
           {/* List of available resources, i.e. police number, how to get an escort, etc. */}
-          <h2>Resources</h2>
+          <h2 className="text-xl font-bold">Resources</h2>
+          {/* police escort service */}
+          <h2 className="">Request a Safety Escort: <a href="tel:5629854101">562.985.4101</a></h2>
+          {/* anonymous tip line */}
+          <a href="https://www.csulb.edu/university-police/form/submit-an-anonymous-crime-tip">Report Crime Tip (Anonymous)</a>
         </div>
       </div>
     </>
