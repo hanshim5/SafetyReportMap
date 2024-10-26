9256145c647a9f866117ef3e1b71def66534ad90
jaerlcruz <jaeraelcruz@gmail.com>
2024-10-26 11:48:49 -0700

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,6 +6,12 @@ function SideBar() {
       <button>
         {/* Button should open AddMarker component for user to input a new marker. */}
       </button>
+      <div>
+        {/* Display details for a marker, if selected. Otherwise, inform user to select a marker to view details. */}
+      </div>
+      <div>
+        {/* List of available resources, i.e. police number, how to get an escort, etc. */}
+      </div>
     </>
   )
 }
