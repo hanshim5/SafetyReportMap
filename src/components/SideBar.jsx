601cb462c74022fc5cde71409aa79933802bd9e9
jaerlcruz <jaeraelcruz@gmail.com>
2024-10-26 12:53:42 -0700

    Added basic sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,15 +3,23 @@ import { useState, useEffect } from 'react';
 function SideBar() {
   return (
     <>
-      <button>
+      <div className="w-1/5 h-3/4 bg-white flex-col m-16 rounded-lg p-4">
+        <button>
         {/* Button should open AddMarker component for user to input a new marker. */}
-      </button>
-      <div>
-        {/* Display details for a marker, if selected. Otherwise, inform user to select a marker to view details. */}
-      </div>
-      <div>
-        {/* List of available resources, i.e. police number, how to get an escort, etc. */}
+        Add Marker
+        </button>
+        <div className="text-black text-xl">
+          {/* Display details for a marker, if selected. Otherwise, inform user to select a marker to view details. */}
+          {/* May have to pass in details component. */}
+          <h2>Incident Details</h2>
+        </div>
+        <div className="text-black">
+          {/* List of available resources, i.e. police number, how to get an escort, etc. */}
+          <h2>Resources</h2>
+        </div>
       </div>
     </>
   )
 }
+
+export default SideBar;
