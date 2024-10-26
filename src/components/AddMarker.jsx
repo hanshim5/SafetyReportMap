7c236666a5d779da3f82d5f48d952898ce7a04b4
jaerlcruz <jaeraelcruz@gmail.com>
2024-10-26 11:40:26 -0700

    Create AddMarker.jsx

---
+++ src/components/AddMarker.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+function AddMarker( ) {
+  return (
+    <>
+    <button>
+      {/* TODO: Button allows user to add marker to the Map. */}
+      {/* Upon marker creation: user can input information about the incident. */}
+    </button>
+    </>
+  )
+};
