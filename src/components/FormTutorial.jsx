0e1223b222a9dd565d7003d920e1e419fd88fe7e
jaerlcruz <jaeraelcruz@gmail.com>
2024-10-27 07:57:28 -0700

    Corrected FormTutorial.jsx

--- src/components/FormTutorial.jsx
+++ src/components/FormTutorial.jsx
@@ -7,7 +7,7 @@ function FormTutorial({}) {
       <div className="fixed w-72 bg-[#3A3431] flex flex-col rounded-lg p-8 right-16 top-64">
       
       <p className="text-gray-300"><span className="font-semibold">To report a new incident: </span>
-            Click "Add Marker" in the left sidebar, then click on a location on the map to add a new marker. Text boxes will appear 
+            Click "Report New Incident" in the left sidebar, then click on a location on the map to add a new marker. Text boxes will appear 
             <span className="font-semibold"> here </span>so you can input details.</p>
 
       </div>
